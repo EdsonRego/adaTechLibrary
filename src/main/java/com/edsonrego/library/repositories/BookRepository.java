c38b1ed58951abc60bcf497f73997ccf256cfb00
EdsonRego <edsonxrego@gmail.com>
2024-03-11 22:48:25 -0300

    Repositories

---
+++ src/main/java/com/edsonrego/library/repositories/BookRepository.java
@@ -0,0 +1,9 @@
+package com.edsonrego.library.repositories;
+
+import com.edsonrego.library.entities.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BookRepository extends JpaRepository<Book, Long> {
+}
